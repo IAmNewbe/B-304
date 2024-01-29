010b274e2077796ada71423e72e733c5ff1e0ab1
IAmNewbe <ahlulhikam2222@gmail.com>
2024-01-29 19:49:59 +0700

    random bullshit go

---
+++ Tes.py
@@ -0,0 +1,7 @@
+from Circle import Circle
+
+objek = Circle(10)
+luas = objek.getArea()
+
+print("luas : ", luas)
+
---
+++ tes.cpp
@@ -0,0 +1,10 @@
+#include  <iostream>
+using namespace std;
+
+int main() {
+  // bool nilai = 0;
+  for (;;) {
+    cout <<"Running"<<endl;
+  }
+  return 0;
+}
